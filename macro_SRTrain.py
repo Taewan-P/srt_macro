252c1d694e8b188aaabce3f0d1db4e813e75e9c1
youngmin yoo <yym3055@gmail.com>
2023-05-24 22:02:16 +0900

    gitignore 정리

--- macro_SRTrain.py
+++ macro_SRTrain.py
@@ -1,34 +1,58 @@
 import configparser
-from SRT import SRT
-import time
+from SRT import SRT, SeatType
+import time, logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', )
 
 # Read the configuration file
-config = configparser.ConfigParser()
-config.read('config.ini')
+def read_config():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    logging.info('read config.ini', config)
+    return config
+    
+def reserve_train(srt, config):
 
-username = config['login']['username']
-password = config['login']['password']
-reservation_date = config['reservation']['date']
-reservation_time = config['reservation']['time']
-departure = config['reservation']['departure']
-destination = config['reservation']['destination']
-train_kind = config['reservation']['train_kind']
+    if 'time_limit' in config['reservation']:
+        trains = srt.search_train(config['reservation']['departure'],
+                               config['reservation']['destination'],
+                                 config['reservation']['date'],
+                                   config['reservation']['time'],
+                                   time_limit = config['reservation']['time_limit'],
+                                     available_only=False)    
+    else:
+        trains = srt.search_train(config['reservation']['departure'],
+                                config['reservation']['destination'],
+                                    config['reservation']['date'],
+                                    config['reservation']['time'],
+                                        available_only=False)
+    logging.info(f"예매를 시도합니다: {trains}")
+    trials = 0
+    while True:
+        for train in trains:
+            try:
+                res = srt.reserve(train, special_seat=SeatType.GENERAL_FIRST)
+                logging.info("reservation successed")
+                break
+            except Exception as e:
+                pass
+        
+        time.sleep(0.1)
+        trials += 1
+        logging.info(f"trial {trials}: Trains full, retrying...")
+        if trials % 10 == 0:    
+            print(f"retrying... {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}")
+    return res
 
-# Create an SRT instance
-srt = SRT(username, password)
+    
 
-# Search for SRT train schedules
-trains = srt.search_train(departure, destination, reservation_date)
+def main():
+    config = read_config()
+    # Create an SRT instance
+    srt = SRT(config['login']['username'], config['login']['password'])
+    res = reserve_train(srt, config)
+    print(res)
 
-# Reserve an SRT train
-while True:
-    try:
-        res = srt.reserve(trains[0])
-        break
-    except Exception as e:
-        print(e)
-        print("Retrying...")
-        time.sleep(0.2)
-
-# Print the reservation information
-print(res)
+if __name__ == '__main__':
+    main()
+    
