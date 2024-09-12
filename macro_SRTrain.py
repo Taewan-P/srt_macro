fadd8a2841e0f405920bf30d7e7a17a43f0ffd48
Taewan Park <swimingtw@gmail.com>
2024-09-12 18:05:19 +0900

    Improve error handling and send discord message when booked

--- macro_SRTrain.py
+++ macro_SRTrain.py
@@ -1,6 +1,10 @@
 import configparser
-from SRT import SRT, SeatType
+import json
 import time, logging
+import requests
+
+from SRT import SRT, SeatType
+from SRT.errors import SRTResponseError
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', )
 
@@ -10,7 +14,7 @@ def read_config():
     config.read('config.ini')
     logging.info('read config.ini', config)
     return config
-    
+
 def reserve_train(srt, config):
 
     if 'time_limit' in config['reservation']:
@@ -19,7 +23,7 @@ def reserve_train(srt, config):
                                  config['reservation']['date'],
                                    config['reservation']['time'],
                                    time_limit = config['reservation']['time_limit'],
-                                     available_only=False)    
+                                     available_only=False)
     else:
         trains = srt.search_train(config['reservation']['departure'],
                                 config['reservation']['destination'],
@@ -32,27 +36,41 @@ def reserve_train(srt, config):
         for train in trains:
             try:
                 res = srt.reserve(train, special_seat=SeatType.GENERAL_FIRST)
-                logging.info("reservation successed")
-                break
-            except Exception as e:
+                logging.info("reservation succeeded")
+                return res
+            except SRTResponseError as e:
+                if e.msg != "잔여석없음":
+                    raise e
+            except json.decoder.JSONDecodeError as e:
                 pass
-        
-        time.sleep(0.6)
+            except Exception as e:
+                logging.error(f"An error occurred: {e}")
+
+        time.sleep(0.8)
         trials += 1
         logging.info(f"trial {trials}: Trains full, retrying...")
-        if trials % 10 == 0:    
-            print(f"retrying... {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}")
+
     return res
 
-    
+def send_message(message: str, url: str, userid: str):
+    data = {
+        "content": f"<@{userid}>",
+        "embeds": [
+            {
+                "title": "SRT Booked",
+                "description": f"{message}"
+            }
+        ]
+    }
+    requests.post(url, json=data)
 
 def main():
     config = read_config()
     # Create an SRT instance
     srt = SRT(config['login']['username'], config['login']['password'])
     res = reserve_train(srt, config)
+    send_message(str(res), config['discord']['url'], config['discord']['userid'])
     print(res)
 
 if __name__ == '__main__':
     main()
-    
